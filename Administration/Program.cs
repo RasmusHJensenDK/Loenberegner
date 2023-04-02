6a179f6407dd2e5feca08aa4f69262ad46af6068
Rasmus <pr1nce@live.dk>
2023-04-02 23:20:44 +0200

    Initial

---
+++ Administration/Program.cs
@@ -0,0 +1,14 @@
+﻿using lb;
+
+Console.WriteLine("Tilføjer 100 timer, 135 i timeløn, 5000 i fradrag, 40% træk");
+
+Loenberegning loen = new Loenberegning(100, 135, 5000, 40);
+
+Console.WriteLine("Løn beregning for denne måned :" + loen.getNettoLoen());
+
+Console.WriteLine("Tilføjer 100 timer, ny antal timer for denne måned : " + loen.TilfoejTimer(100));
+
+Console.WriteLine("Løn beregning for denne måned :" + loen.getNettoLoen());
+
+
+Console.ReadKey();
---
+++ lb/Loenberegning.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Administration")]
+
+namespace lb
+{
+    internal class Loenberegning
+    {
+        private static double bruttoloen { get; set; }
+        private static double ambidrag { get; set; }
+        private static double bruttoloenminusambidrag { get; set; }
+        private static double loenminusfradrag { get; set; }
+        private static double skat { get; set; }
+        private static double nettoloen { get; set; }
+        private static double at { get; set; }
+        private static double tl { get; set; }
+        private static double fd { get; set; }
+        private static double sp { get;set; }
+        internal Loenberegning(double antalTimer, double timeLøn, double fradrag, double skatteProcent)
+        {
+            at = antalTimer;
+            tl = timeLøn;
+            fd = fradrag;
+            sp = skatteProcent;
+        }
+        internal void BeregnLoen()
+        {
+            bruttoloen = at * tl;
+            ambidrag = 0.08 * bruttoloen;
+
+            bruttoloenminusambidrag = bruttoloen - ambidrag;
+            loenminusfradrag = bruttoloenminusambidrag - fd;
+
+            skat = sp * loenminusfradrag / 100;
+            nettoloen = bruttoloenminusambidrag - skat;
+        }
+        internal string TilfoejTimer(double timerTilføjes)
+        {
+            at = at + timerTilføjes;
+            return at.ToString();
+        }
+        internal double getNettoLoen()
+        {
+            BeregnLoen();
+            return nettoloen;
+        }
+    }
+}
